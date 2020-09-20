7c40d76b05c54464917acdf2cc25a6c9ef660224
dariopregnolato <68358497+dariopregnolato@users.noreply.github.com>
2020-09-20 12:07:58 -0300

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,16 @@
+<?php 
+
+$nome = $_POST['tNome'];
+$email = $_POST['tMail'];
+$cpf = $_POST['tCPF'];
+$telefone = $_POST['tTel'];
+$cep = $_POST['tCEP'];
+$largadouro = $_POST['tLargadouro'];
+$numero = $_POST['tNumero'];
+$bairro = $_POST['tBairro'];
+$cidade = $_POST['tCidade'];
+$estado = $_POST['tEst'];
+
+echo "Seu nome é $nome, seu email é $email";
+
+ ?>
